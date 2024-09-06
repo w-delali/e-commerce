bcd222da0302db476661641f18080f36dc7a4e05
Bright Delali Wordi <brightdelaliwordi@gmail.com>
2024-09-06 19:03:25 +0000

    modified prisma client and schema

---
+++ prisma/migrations/20240906182827_added_tags_to_product/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE "users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "tags" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,29 +1,16 @@
-import {PrismaClient} from "@prisma/client";
-import {Prisma} from "prisma/prisma-client/scripts/default-index";
-import {hashPassword} from "../src/utils/auth-utils";
+import { PrismaClient } from "@prisma/client";
+import { hashPassword } from "../src/utils/auth-utils";
 
-const prismaClient = new PrismaClient(
-    {log: ["query"]}
-).$extends({
-    query:{
-        user:{
-            create:async ({args,query})=>{
-                const newArgs= args
-                newArgs.data.password=await hashPassword(newArgs.data.password)
-            return query(newArgs)
-            }
-        }
-    }
-})
+const prismaClient = new PrismaClient({ log: ["query"] }).$extends({
+  query: {
+    user: {
+      create: async ({ args, query }) => {
+        const newArgs = args;
+        newArgs.data.password = await hashPassword(newArgs.data.password);
+        return query(newArgs);
+      },
+    },
+  },
+});
 
-const hashPasswordExt =Prisma.defineExtension({
-    query:{
-        user:{
-
-        }
-    }
-})
-
-
-
-export  {prismaClient}
+export { prismaClient };
